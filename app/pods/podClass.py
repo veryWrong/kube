2240fb133610c190c303c5dafd08a1b3ac542bba
hotel <471752785@qq.com>
2018-07-13 18:11:02 +0800

    exec

---
+++ app/pods/podClass.py
@@ -0,0 +1,23 @@
+from kubernetes import client
+from flask_login import current_user
+
+
+class Pod(object):
+    def __init__(self, api_client=None):
+        if api_client is None:
+            api_client = client.CoreV1Api()
+        self.api_client = api_client
+        self.username = current_user.username
+
+    def all_list(self):
+        pods = self.api_client.list_pod_for_all_namespaces(watch=False)
+        return pods
+
+    def get_list(self):
+        pods = self.api_client.list_namespaced_pod(self.username)
+        return pods
+
+    def exec(self):
+        name = 'flaskdeploy-55c586b7bb-cc5wv'
+        res = self.api_client.connect_post_namespaced_pod_exec(name, self.username, tty=True)
+        return res
--- app/pods/pods.py
+++ app/pods/pods.py
@@ -1,14 +1,13 @@
-from kubernetes import client
 from flask import jsonify
 from flask_login import login_required
+from .podClass import Pod
 from . import pod
 
 
 @pod.route('/', methods=['GET', ])
 @login_required
 def pod_count():
-    v1 = client.CoreV1Api()
-    ret = v1.list_pod_for_all_namespaces(watch=False)
+    ret = Pod().all_list()
     online, offline = 0, 0
     for i in ret.items:
         item = i.status.container_statuses[-1]
@@ -21,3 +20,11 @@ def pod_count():
         'online': online,
         'offline': offline,
     }})
+
+
+@pod.route('/exec', methods=['POST', ])
+@login_required
+def tty():
+    res = Pod().exec()
+    print(res)
+    return jsonify({'code': 200, 'msg': 'ok'})
